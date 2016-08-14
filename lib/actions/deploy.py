dabed82ce3a6a693837acb3d0b7992ff262437bc
Evan Kaufman <evan@digitalflophouse.com>
2016-08-14 10:40:33 -0700

    Tweaks to `evolve` wrapper, `mock` wrapper, generator, and group vars
    In `evolve` wrapper:
    * resolving paths to absolute
    * using pre-initialized extra_vars dict in actions
    
    In `mock` wrapper, overriding repo url
    
    In generator, updated fixPermissions to ensure binaries are executable
    
    In group vars:
    * updated deploy path
    * fixed ansistrano before/after playbook paths

--- lib/actions/deploy.py
+++ lib/actions/deploy.py
@@ -4,20 +4,8 @@ import sys
 
 class ActionClass:
     def __init__(self, evolve):
-        extra_vars = {
-            'stage': evolve.stage,
-            'deploy_mode': 'deploy'
-        }
-
-        # use branch from extra_vars, if given
-        if evolve.arguments.extra_vars and 'branch' in evolve.arguments.extra_vars:
-            extra_vars['branch'] = evolve.arguments.extra_vars['branch']
-        # otherwise, for non-prod stages, infer currently checked out branch
-        elif evolve.stage != 'production':
-            result = evolve.bg_call(['git', 'branch'])
-            matched = re.search(r'^[*] (\S+)\s', result['stdout'], re.M)
-            if matched and matched.group(1) != 'master':
-                extra_vars['branch'] = matched.group(1)
+        extra_vars = evolve.get_extra_vars()
+        extra_vars['deploy_mode'] = 'deploy'
 
         # infer git remote from currently checked out repo
         git_remote = evolve.bg_call(['git', 'config', '--get', 'remote.origin.url'])
--- lib/actions/provision.py
+++ lib/actions/provision.py
@@ -4,16 +4,13 @@ import subprocess
 class ActionClass:
     def __init__(self, evolve):
         # TODO: implement `invoke "evolve:prepare_key"`
-
-        extra_vars = {
-            'stage': evolve.stage
-        }
+        extra_vars = evolve.get_extra_vars()
 
         try:
             evolve.playbook('provision.yml', extra_vars, '--user=deploy')
         except subprocess.CalledProcessError as err:
             # only prompt for user reprovisioning if host failed or unreachable
-            if any(x in err for x in ['returned non-zero exit status 3', 'returned non-zero exit status 2']):
+            if 'returned non-zero exit status 3' in str(err):
                 evolve.announce('Unable to provision with SSH publickey for deploy user')
 
                 # connect as intermediate user, and set up deploy user
